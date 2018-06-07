92ba72c091126e324b4e09dcffc229dd02341628
Yurockkk <r00631026@ntu.edu.tw>
2018-06-07 12:30:04 -0400

    Adding Ingredient[] to Recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
-    
+
     private recipes: Recipe[] = [
-    new Recipe("A Test Recipe","This is simply a test","https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=800&q=85"),
-    new Recipe("The second Test Recipe","This is simply a test","https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=800&q=85")
+        new Recipe(
+            "A Test Recipe",
+            "This is simply a test",
+            "https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=800&q=85",
+            [
+                new Ingredient('meat',1),
+                new Ingredient('French Fries',20)
+            ]
+        ),
+        new Recipe(
+            "Big Fat Burger",
+            "This is simply a test",
+            "https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2Frecipes%2Fck%2F11%2F04%2Ffettuccine-olive-oil-ck-x.jpg%3Fitok%3DN9u99OOY&w=800&q=85",
+            [
+                new Ingredient('Buns',2),
+                new Ingredient('meat',1)
+            ]
+        )
     ];
 
     getRecipes(){
