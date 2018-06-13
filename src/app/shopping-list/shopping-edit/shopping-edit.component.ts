57b8e676511e26a136167b15597bbfa1daed9689
Yurockkk <r00631026@ntu.edu.tw>
2018-06-13 17:52:50 -0400

    Refactor shopping-edit form using template-driven approach

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { Ingredient } from '../../shared/ingredient.model';
+import { Component, OnInit } from '@angular/core';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -11,19 +12,18 @@ export class ShoppingEditComponent implements OnInit {
 
   //two different ways to get value from input element,
   //1. get input value by ViewChild
-  @ViewChild('nameInput') nameInput: ElementRef;
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
   //2. get input value by passing it as a param
-  onAddItem(amountInput: HTMLInputElement){
-    console.log(`onSubmitted, name: ${this.nameInput.nativeElement.value}, amount: ${amountInput.value}`);
-    const ingName = this.nameInput.nativeElement.value;
-    const ingAmount = Number(amountInput.value);
-    const newIngredient = new Ingredient(ingName,ingAmount);
+  onAddItem(form: NgForm){
+    const value = form.value;
+    console.log(`onSubmitted, name: ${value.name}, amount: ${value.amount}`);
+    const newIngredient = new Ingredient(value.name,value.amount);
     this.shoppingListService.addIngredient(newIngredient);
   }
+  
 
 }
